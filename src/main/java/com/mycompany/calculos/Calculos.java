47232139ac3294473e6f895a7788a172f9e03810
LuisHurtadoM <luisofo@me.com>
2024-05-01 11:13:19 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/calculos/Calculos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculos;
+
+/**
+ *
+ * @author Luis
+ */
+public class Calculos {
+
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar (15f,4f));
+    }
+}
